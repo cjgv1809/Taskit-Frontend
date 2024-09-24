f2ab1cf21de905e78fa62df7d75cba82562a48a8
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-09-23 22:21:08 -0300

    add react-router-dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import HomeView from "./views/HomeView";
+import SignInView from "./views/SignInView";
+import RegisterView from "./views/RegisterView";
+import NotFoundView from "./views/NotFoundView";
 
 function App() {
   return (
     <>
-      <HomeView />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/sign-in" element={<SignInView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
     </>
   );
 }
