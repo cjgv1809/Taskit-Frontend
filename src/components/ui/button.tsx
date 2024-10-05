106827b355bb18979f69039c7e627333447d63ca
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-10-05 11:43:37 -0300

    change some classes in Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border border-secondary font-bold bg-background shadow-sm hover:bg-secondary hover:text-white",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "text-primary-foreground hover:text-primary-foreground/80",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
